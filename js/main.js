879cba7275b4be790827c03a70cfcb47ac2fdb3e
sakimastringer <sakimastringer@gmail.com>
2023-03-09 19:01:31 -0600

    not mvp still in progress

--- js/main.js
+++ js/main.js
@@ -19,10 +19,10 @@ let match
 let win
 
 
-let main = getRandomItem
+// let main = getRandomItem //
 
 
-// cached elements //
+// cached elements //'
 
 // selecting h1 element //
 const messageEl = document.querySelector('h1')
@@ -30,13 +30,17 @@ const messageEl = document.querySelector('h1')
 const playAgainButton = document.querySelector('button')
 // selecting square element //
 const choiceEls = document.querySelectorAll('.square')
+// selecting Main Image //
+const mainImageEl = document.getElementById('.mainImage')
+// selecting timer //
+const timerEl = document.getElementById('timer')
 console.log(choiceEls)
 
 
 // event listeners //
 // Attached method using variable name //
 playAgainButton.addEventListener('click', resetChoices)
-
+ console.log()
 
 
 // functions //
@@ -66,26 +70,42 @@ function renderChoices() {
     })
 }
 
+
+
 // Declaring function //
 function resetChoices() {
     choices = []
     console.log('test')
-    // calling the render
+    // calling the render //
     render()
 }
 
+var count = 9, shotClock= setInterval(function() {
+    ("counter")(count-1);
+    if(count == 0) clearInterval(shotClock);
 
-// function getRandomItem(str) {
-//     const randomIdx = Math.floor(Math.random() * str.length)
-//     const item = str[randomIdx]
+}, 1000);
 
-//     return item
+// function matchImages() {
+//     // Match options are from the string referenced above //
+//     const options = "tileColors".length
+//     if (options === 2)
+//     // if(mainImage === picked Image)matched
+//     // Everything else other than above line in not a match //   
+//     else {unmatched()}
 // }
 
-// let str = choices
+function getRandomItem(str) {
+    const randomIdx = Math.floor(Math.random() * str.length)
+    const item = str[randomIdx]
 
-// const result = getRandomItem(str)
-// console.log(result)
+    return item
+}
+
+let str = choices
+
+const result = getRandomItem(str)
+console.log(result)
 
 
 
